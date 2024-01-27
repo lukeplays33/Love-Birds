560440251b08dcb586a4948d20e93e537d1eaed6
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-01-27 11:04:42 +0000

    test

--- server.js
+++ server.js
@@ -12,10 +12,19 @@ const PORT = 3000;
 const HOST = '0.0.0.0';
 
 // App
-const app = express();
-app.get('/', (req, res) => {
-	res.send('Hello remote world!\n');
+var fs = require('fs');
+var sys = require('sys');
+
+var app = express();
+    app.use(express.logger());
+    app.set("view options", {layout: false});
+    app.use(express.static(__dirname + '/html'));
+
+app.get('/', function(req, res){
+    res.render('/html/index.html');
 });
+
+app.listen(8080);
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
