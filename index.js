44a0a3b73e3a2e79bcb11a3b5f5e4138c84017ff
= <lukehoogenboom@gmail.com>
2024-02-19 15:58:18 +0100

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { net } from "./js/main.js";
 
 function generateResponse(input) {
     const output = net.run(input);
+    console.log(output)
     document.getElementById('response').innerHTML = output;
     // You can use or display the generated response in the UI or perform any other action here
   }
