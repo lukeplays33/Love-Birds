ea48fac091319e9f25c86b4c534f4e045bc4ad21
= <lukehoogenboom@gmail.com>
2024-02-19 17:25:47 +0100

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function generateResponse(input) {
   const output = net.run(input);
 
   const responseId = Object.keys(output)[0];
-  const responseMessage = dictionary.find((message) => message.id === parseInt(responseId));
+  const responseMessage = responseId
 
   document.getElementById('response').innerHTML = responseMessage;
   // You can use or display the generated response in the UI or perform any other action here
