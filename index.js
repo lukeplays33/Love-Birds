ee0145fe34f05953fd27cbe10e76c0ffdc864e68
= <lukehoogenboom@gmail.com>
2024-02-19 15:45:38 +0100

    test

---
+++ index.js
@@ -0,0 +1,9 @@
+function generateResponse(input) {
+    const output = net.run(input);
+    document.getElementById('response').innerHTML = output;
+    // You can use or display the generated response in the UI or perform any other action here
+  }
+
+document.getElementById('msg').onsubmit = function () {
+    generateResponse({ type: 'greeting', content: this.value });
+}
