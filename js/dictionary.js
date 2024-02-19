3f1c8be9b6f0db5d32e3a2bdd82de7a9bffac936
= <lukehoogenboom@gmail.com>
2024-02-19 17:14:45 +0100

    test

--- js/dictionary.js
+++ js/dictionary.js
@@ -1,6 +1,6 @@
-let dictionary = [{
-    input: { type: 'greeting', content: 'Hello' },
-    output: { response: 'Hi there!' }
-}];
+let dictionary = [  { id: 1, message: 'Hello' },
+{ id: 2, message: 'How are you?' },
+{ id: 3, message: 'What is your favorite color?' },
+// Add more user messages here];
 
 export { dictionary }
