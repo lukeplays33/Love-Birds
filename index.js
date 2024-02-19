77489aadcff91f27d9bdcfbef64e5acb039b2134
= <lukehoogenboom@gmail.com>
2024-02-19 17:21:59 +0100

    test

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import { net } from "./js/main.js";
 
 function generateResponse(input) {
-    net.run(input);
+  const output = net.run(input);
 
-    const responseId = Object.keys(output)[0];
-const responseMessage = userMessages.find((message) => message.id === parseInt(responseId));
+  const responseId = Object.keys(output)[0];
+  const responseMessage = userMessages.find((message) => message.id === parseInt(responseId));
 
-    document.getElementById('response').innerHTML = responseMessage;
-    // You can use or display the generated response in the UI or perform any other action here
-  }
+  document.getElementById('response').innerHTML = responseMessage;
+  // You can use or display the generated response in the UI or perform any other action here
+}
 
 document.getElementById('msg').onchange = function () {
-    generateResponse({ type: 'greeting', content: this.value });
+  generateResponse({ type: 'greeting', content: this.value });
 }
