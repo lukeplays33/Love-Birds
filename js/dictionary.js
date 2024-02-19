8fb267f49b68002ae88faeeedd5e1b1ac6fd6b91
= <lukehoogenboom@gmail.com>
2024-02-19 17:16:25 +0100

    forgor ]

--- js/dictionary.js
+++ js/dictionary.js
@@ -1,6 +1,8 @@
-let dictionary = [  { id: 1, message: 'Hello' },
+let dictionary = [  
+    { id: 1, message: 'Hello' },
 { id: 2, message: 'How are you?' },
 { id: 3, message: 'What is your favorite color?' },
+}
 // Add more user messages here];
 
 export { dictionary }
