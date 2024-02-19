32208c3320bb3075badb1fdfba11282540471b65
= <lukehoogenboom@gmail.com>
2024-02-19 17:17:33 +0100

    ugh

--- js/dictionary.js
+++ js/dictionary.js
@@ -2,7 +2,7 @@ let dictionary = [
     { id: 1, message: 'Hello' },
 { id: 2, message: 'How are you?' },
 { id: 3, message: 'What is your favorite color?' },
-}
+]
 // Add more user messages here];
 
 export { dictionary }
