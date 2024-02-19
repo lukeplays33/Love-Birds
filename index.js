bb8e159896f6d3bcb0df817f54d7c11e8047642a
= <lukehoogenboom@gmail.com>
2024-02-19 16:57:35 +0100

    test

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 import { net } from "./js/main.js";
 
 function generateResponse(input) {
-    const output = net.run({ type:"greeting", content: encodeText(input.content) });
+    const output = net.run({ type:"greeting", content: input.content });
     console.log(output)
     document.getElementById('response').innerHTML = output;
     // You can use or display the generated response in the UI or perform any other action here
-  }
-
-  function encodeText(text) {
-    return text.split('').map(char => char.charCodeAt(0) / 255);
   }
 
 document.getElementById('msg').onchange = function () {
