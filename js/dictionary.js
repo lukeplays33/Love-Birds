fae4342e0c4de8390015bcd2b893c68665ae4ef2
= <lukehoogenboom@gmail.com>
2024-02-19 17:12:15 +0100

    test

--- js/dictionary.js
+++ js/dictionary.js
@@ -1,6 +1,6 @@
-let dictionary = [    {
+let dictionary = [{
     input: { type: 'greeting', content: 'Hello' },
     output: { response: 'Hi there!' }
-  }]
+}];
 
 export { dictionary }
--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 import { dictionary } from "./dictionary.js";
 
-console.log(dictionary)
+console.log(dictionary);
 
 const net = new brain.NeuralNetwork();
-
-net.train(dictionary);
+  
+  // Train the chatbot with user messages
+  dictionary.forEach((message) => {
+    net.train([{ input: message.message, output: message.id }]);
+  });
 
 export { net }; 
